134d0a232d36bcd8f9d13432e95693b8c973b1ad
Moksh Chaudhary <moksh4445@gmail.com>
2020-03-21 23:04:14 +0530

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Fast-Typer
-A bot to play the Fast-Typer type Games
+A bot to play the [Fast-Typer](https://poki.com/en/g/fast-typer-3).
+
+![Demo](https://media.giphy.com/media/JUjQh63Xjz9W8YR0Ve/giphy.gif)
+
+### Requirements:
+
+- OpenCv
+- Pytesseract
+- Numpy
+- PyautoGui
+- MSS
+
+### Notes:
+
+The region can change depending on the resolution of the screen.
+
+Due to the nature of OCR the bot doesn't have 100% accuracy.
--- fast_typer.py
+++ fast_typer.py
@@ -5,14 +5,13 @@ import cv2
 import numpy as np
 import pytesseract
 
-con = r'--psm 7 -l eng -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwyz'
+con = r'--psm 7 -l eng -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz'
 while(True):
     time.sleep(0.5)
     with mss.mss() as sct:
                 monitor = {"top": 341, "left": 550, "width": 522, "height": 90}  #Defines the Region
                 img=sct.grab(monitor) 
     img = np.array(img)
-    #img = cv2.imread('4.png')
     img= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     (thresh, im_bw) = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     img=cv2.GaussianBlur(im_bw, (5, 5), 0)
